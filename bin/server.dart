ebc03dd241d0e29136c1ff67325f56ec6d27fcfa
Seth Ladd <sethladd@gmail.com>
2014-01-19 13:29:37 -0800

    Use method cascades for server response

--- bin/server.dart
+++ bin/server.dart
@@ -5,11 +5,12 @@ echo(HttpRequest req) {
   print('received submit');
   HttpBodyHandler.processRequest(req).then((HttpBody body) {
     print(body.body.runtimeType); // Map
-    req.response.headers.add('Access-Control-Allow-Origin', '*');
-    req.response.headers.add('Content-Type', 'text/plain');
-    req.response.statusCode = 201;
-    req.response.write(body.body.toString());
-    req.response.close();
+    req.response
+      ..headers.add('Access-Control-Allow-Origin', '*')
+      ..headers.add('Content-Type', 'text/plain')
+      ..statusCode = 201
+      ..write(body.body.toString())
+      ..close();
   });
 }
 
